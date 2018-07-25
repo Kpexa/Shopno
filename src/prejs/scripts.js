41f085e4491ee41851fe7e55b309f5ebeac35fde
Kpexa <33284586+Kpexa@users.noreply.github.com>
2018-07-25 12:40:13 +0300

    made menu nutton toogleble

---
+++ src/prejs/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  $('.burger').on('click', function(){
+    $(this).fadeOut(500);
+    $('.close').fadeIn(500);    
+  });
+
+  $('.close').on('click', function(){
+    $(this).fadeOut(500); 
+    $('.burger').fadeIn(500);
+  });
+
+});
