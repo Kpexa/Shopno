222607762ea03a207a193ee3bbe3f769057621d2
Kpexa <33284586+Kpexa@users.noreply.github.com>
2018-08-06 14:49:13 +0300

    delete old script

--- src/prejs/scripts.js
+++ src/prejs/scripts.js
@@ -1,13 +1,3 @@
 $(function(){
-
-  $('.burger').on('click', function(){
-    $(this).fadeOut(500);
-    $('.close').fadeIn(500);        
-  });
-
-  $('.close').on('click', function(){
-    $(this).fadeOut(500); 
-    $('.burger').fadeIn(500);               
-  });
-
+  
 })
