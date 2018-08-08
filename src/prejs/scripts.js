76d82e89afca57a63caa14a68386c42349d62fa5
Kpexa <33284586+Kpexa@users.noreply.github.com>
2018-08-08 11:36:23 +0300

    upgrade menu
    made menu items clickable (use JS (jQuery))
    
    appropriate menu items become active style on scroll event or on click

--- src/prejs/scripts.js
+++ src/prejs/scripts.js
@@ -1,3 +1,53 @@
 $(function(){
-  
-})
+
+  let links = $('.menu-items a');
+  let menu_height = parseInt($('.menu-items').height());
+
+  links.on('click', function(e){
+
+    e.preventDefault();
+
+    links.removeClass('active-item');
+    let active_item = $(this).addClass('active-item');
+    let selector = active_item.attr('href');
+    let target = $(selector);
+
+    if(target.length > 0) {
+      $('html, body').animate({
+        scrollTop: target.offset().top - menu_height
+      }, 500);
+    }
+
+  });
+
+  let scrollTimer;
+
+  $(document).on('scroll', function(){
+
+    clearInterval(scrollTimer);
+
+    scrollTimer = setTimeout(() => {
+      selectActiveMenuItem.call(this);
+    }, 100);
+
+  });
+
+  function selectActiveMenuItem() {
+
+    let current_scroll = $(this).scrollTop();
+
+    links.each(function(){
+      let a = $(this);
+      let selector = a.attr('href');
+      let target = $(selector);
+
+      if((target.offset().top < current_scroll + 400)) {
+        links.removeClass('active-item');
+        $(this).addClass('active-item');
+      }
+
+    });
+
+  }
+
+});
